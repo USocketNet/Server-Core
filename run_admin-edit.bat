bb6bbb2214586f1a5c56a4b3187b5a19fe28266d
Bytes Crafter <bytescrafter@gmail.com>
2019-09-20 00:30:01 +0800

    Deleted all the custom windows batch file.

--- run_admin-edit.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-TITLE USocket.Net Server v1.0.0 : Bytes Crafter @ 2018
-mode con: cols=125 lines=84
-%CD:~0,2%
-cd %CD%
-start notepad "server_library/configs/admin.json"
-exit
--- run_npm-install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-TITLE USocketNet Server : Bytes Crafter @ 2018
-mode con: cols=125 lines=84
-%CD:~0,2%
-cd %CD%
-npm install
-npm -v
-PAUSE
