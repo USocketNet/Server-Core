9d00d6d19e7cff4ae8c718e44dd781a1a5b849ff
Bytes Crafter <bytescrafter@gmail.com>
2019-11-18 13:45:17 +0800

    Remove demo on node public html.

--- public_html/demoguy/assets/js/app.js
+++
@@ -1,6 +0,0 @@
-var socket = io('http://localhost:8080');
-
-socket.on('welcome', function (data) {
-    console.log('Token: ' + data.token);
-    socket.emit('session', { contype: 'input', chanid : $_GET['cid'] });
-});
