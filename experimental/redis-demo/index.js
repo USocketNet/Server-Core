588accf207c24034fcc03e3902f9857a817c6c95
Bytes Crafter <bytescrafter@gmail.com>
2019-11-18 13:48:17 +0800

    Updated the redis demo server to new nginx html.

--- experimental/redis-demo/index.js
+++ experimental/redis-demo/index.js
@@ -1,33 +1,32 @@
 var express = require('express');
 var app = express();
-    app.use( express.static( __dirname + '/public_html' ) );
+    //Enable this to your server with interface.
+    //app.use( express.static( __dirname + '/public_html' ) );
 var server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 var test = server.listen( process.env.PORT || 3000 );
 const redisAdapter = require('socket.io-redis');
 io.adapter(redisAdapter({ host: 'localhost', port: 6379 }));
 
-io.emit('hello', 'to all clients');
-io.to('room42').emit('hello', "to all clients in 'room42' room");
+//io.emit('hello', 'to all clients');
 
 io.on('connection', (socket) => {
-  console.log('Connected! ' + socket.id + ' @ ' + test.address().port);
+  //Check socket.id of this connection.
+  console.log('Connected! ' + socket.id);
 
+  //Called by client that its connected.
   socket.on('connected', (data, cback) => {
-    console.log('Connetion from ' + data);
-    cback(test.address().port);
+    console.log(data);
+    cback('Im from port ' + test.address().port + '.');
   });
 
-  socket.on('messaged', () => {
-    console.log('Message from ' + test.address().port);
+  socket.on('hello', (data) => {
+    console.log(data);
+    socket.broadcast.emit('hello', 'Hello from server by ' + socket.id);
   });
 
-  socket.on('debuged', (data) => {
-    console.log('Debug from ' + data);
-  });
-
-  socket.broadcast.emit('hello', 'to all clients except sender catered by');
-  socket.to('room42').emit('hello', "to all clients in 'room42' room except sender");
+  //
+  //socket.to('room42').emit('hello', "to all clients in 'room42' room except sender");
 });
 
 console.log('Server started! @' + test.address().port);
