d98fe89ef1c85b561d77dde2b38b86ca1ca32266
Bytes Crafter <bytescrafter@gmail.com>
2019-11-18 11:05:37 +0800

    Remove other package mysql and php.

--- nginx_demo/check.bat
+++ nginx_demo/check.bat
@@ -1,8 +1,6 @@
 @ECHO OFF
+
 echo CHECKING NGINX...
 tasklist /fi "imagename eq nginx.exe"
-echo CHECKING PHP...
-tasklist /fi "imagename eq php-cgi.exe"
-echo CHECKING MYSQL...
-tasklist /fi "imagename eq mysqld.exe"
+
 PAUSE
