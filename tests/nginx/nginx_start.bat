afe91cb418463184644cce0294f1d58936e50b48
Bytes Crafter <bytescrafter@gmail.com>
2019-11-19 13:23:47 +0800

    Added mysql database to NAMP stack.

--- tests/nginx/nginx_start.bat
+++ tests/nginx/nginx_start.bat
@@ -7,6 +7,19 @@ echo STARTING PHP...
 set PATH=./php;%PATH%
 start ./tools/RunHiddenConsole.exe ./php/php-cgi.exe -b 127.0.0.1:9123
 
+echo STARTING MYSQL...
+start ./tools/RunHiddenConsole.exe ./mysql/bin/mysqld --defaults-file=mysql\bin\my.ini --standalone --console
+
+if errorlevel 1 goto error
+goto finish
+
+:error
+echo.
+echo MySQL could not be started
+pause
+
+:finish
+
 PING 127.0.0.1 - 1>NULL
 PING 127.0.0.1 >NULL
 PAUSE
--- tests/nginx/nginx_stop.bat
+++ tests/nginx/nginx_stop.bat
@@ -4,5 +4,7 @@ echo STOPPING NGINX
 taskkill /f /im nginx.exe
 echo STOPPING PHP
 taskkill /f /im php-cgi.exe
+echo STOPPING MYSQL
+taskkill /f /im mysqld.exe
 
 PAUSE
