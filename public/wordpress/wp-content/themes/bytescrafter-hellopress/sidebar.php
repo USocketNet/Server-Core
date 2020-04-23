c1032ad1fdc940002067775eed7334a235734ca1
BytesCrafter <bytescrafter@gmail.com>
2020-04-23 23:30:32 +0800

    We dont require searchform on sidebar in hellopress.

--- public/wordpress/wp-content/themes/bytescrafter-hellopress/sidebar.php
+++ public/wordpress/wp-content/themes/bytescrafter-hellopress/sidebar.php
@@ -13,7 +13,7 @@
     <?php do_action( 'before_sidebar' ); ?>
         <?php if ( is_active_sidebar( 'primary' ) ) : ?>
             <aside id="search" class="widget widget_search">
-                <?php get_search_form(); ?>
+                <?php //get_search_form(); we dont need this because of widget. ?>
             </aside>
             <?php dynamic_sidebar( 'primary' ); ?>
         <?php endif; ?>
