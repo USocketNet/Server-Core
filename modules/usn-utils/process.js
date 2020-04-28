ff8717fb32e9f5dbad25ffb291dd3e7eaf653ba8
BytesCrafter <bytescrafter@gmail.com>
2020-04-28 11:38:11 +0800

    Fixed process not following config file.

--- modules/usn-utils/process.js
+++ modules/usn-utils/process.js
@@ -1,20 +1,23 @@
 
 const debug = require('./debug').init();
+const config = require('./config').init();
 
 class usn_process {
 
     constructor () {
-        // Server process debugging. Show a warning about the uncaught exceptions.
-        process.on('uncaughtException', function (uerror) {
-            debug.log( 'UncaughtException', + uerror.stack + ' @ ' + uerror, 'red', 'process' );
-        });
-        process.stdin.resume(); //Instead of exiting or close server, resume.
-        process.on("beforeExit", () => 
-            debug.log('BeforeExit', 'Received warning that the node server application is trying to shutdown!', 'red', 'process')
-        );
-        process.on("exit", () => 
-            debug.log('Exitting', 'Node server application was shutdown for some reason!', 'red', 'process')
-        );
+        if(config.safe('production', false)) {
+            // Server process debugging. Show a warning about the uncaught exceptions.
+            process.on('uncaughtException', function (uerror) {
+                debug.log( 'UncaughtException', + uerror.stack + ' @ ' + uerror, 'red', 'process' );
+            });
+            process.stdin.resume(); //Instead of exiting or close server, resume.
+            process.on("beforeExit", () => 
+                debug.log('BeforeExit', 'Received warning that the node server application is trying to shutdown!', 'red', 'process')
+            );
+            process.on("exit", () => 
+                debug.log('Exitting', 'Node server application was shutdown for some reason!', 'red', 'process')
+            );
+        }
     }
 }
 
