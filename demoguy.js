95bbced114b458e014bdf1ef1c63195b0eb8d8a3
BytesCrafter <bytescrafter@gmail.com>
2020-04-29 15:53:35 +0800

    Added script header into demoguy script.

--- demoguy.js
+++ demoguy.js
@@ -1,3 +1,15 @@
+
+/*
+    * Package: USocketNet
+    * Description: Self-Host Realtime Multiplayer Server 
+    *       for your Game or Chat Application.
+    * Package-Website: https://usocketnet.bytescrafter.net
+    * 
+    * Author: Bytes Crafter
+    * Author-Website:: https://www.bytescrafter.net/about-us
+    * License: Copyright (C) Bytes Crafter - All rights Reserved. 
+*/
+
 //Require our custom Express.
 const express = require('usn-libs').express;
 const instance = express.init();
